5088afcc24848518b517241851439b3259da3ce3
quang <thaiquang816@gmail.com>
2019-10-30 20:16:56 +0700

    opp

---
+++ ConsoleApp3/ConsoleApp3/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Circle
+    {
+        private double radius;
+        private string color;
+
+        public Circle()
+        {
+            radius = 1.0;
+            color = "red";
+        }
+        public Circle(double r)
+        {
+            radius = r;
+            color = "red";
+        }
+        public Circle(double r, string c)
+        {
+            radius = r;
+            color = c;
+        }
+        public void setRadius(double NewRadius)
+        {
+            radius = NewRadius;
+        }
+        public void setColor(string NewColor)
+        {
+            color = NewColor;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public double getCircumference()
+        {
+            return radius * 2 * Math.PI;
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public override string ToString()
+        {
+            return String.Format("Circle [radius = {0},color = {1}]", getRadius(), getColor());
+            return String.Format("Circle [radius = {0}]", getRadius());
+            //return base.ToString();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine("Circumference of Circle is " + c1.getCircumference());
+            Console.WriteLine(c1.ToString());
+
+            Rectangle r1 = new Rectangle();
+            r1.setLength(5);
+            r1.setWidth(5);
+            Console.WriteLine("Perimeter of Rectangle is " + r1.getPerimeter());
+            Console.WriteLine("Area of Rectangle is " + r1.getArea());
+            Console.WriteLine(r1.ToString());
+            //   c1.setRadius(5.0);
+            //   c1.setColor("Brown");
+            //   Console.WriteLine("The circle has radius of " 
+            //+ c1.getRadius() + " and area of " + c1.getArea()+ " and color is "+ c1.getColor());
+            //   Console.WriteLine();
+            //   Circle c2 = new Circle(3);
+            //   Console.WriteLine("The circle has radius of " 
+            //+ c2.getRadius() + " and area of " + c2.getArea());
+            //   Console.WriteLine();
+            //   Circle c3 = new Circle(3, "Orange");
+            //   Console.WriteLine("The circle color is " + c3.getColor());
+            //   Console.WriteLine(c3.ToString());
+
+
+
+        }
+    }
+}
