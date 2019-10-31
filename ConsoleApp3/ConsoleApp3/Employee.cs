2cc53a6323cab7e880a3627faca52973375deef9
quang <thaiquang816@gmail.com>
2019-10-31 11:41:26 +0700

    opp

---
+++ ConsoleApp3/ConsoleApp3/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public class Employee
+    {
+        private int id, salary;
+        private string firstname, lastname;
+
+        public Employee(int id,int salary,string firstname,string lastname)
+        {
+            this.id = id;
+            this.salary = salary;
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public int getID()
+        {
+            return id;
+        }
+        public string getFirstName()
+        {
+            return firstname;
+        }
+        public string getLastName()
+        {
+            return lastname;
+        }
+        public int getSalary()
+        {
+            return salary;
+        }
+        public void setSalary(int Salary)
+        {
+            salary = Salary;
+        }
+        public int getAnnualSalary()
+        {
+            return salary * 12;
+        }
+        //public int raiseSalary(int percent)
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return string.Format("Employee[id=?,name={0}{1},salary={2}]",getFirstName(),getLastName(),getSalary());
+
+        }
+    }
+}
--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,9 +10,9 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            Circle c1 = new Circle();
-            Console.WriteLine("Circumference of Circle is " + c1.getCircumference());
-            Console.WriteLine(c1.ToString());
+            //Circle c1 = new Circle();
+            //Console.WriteLine("Circumference of Circle is " + c1.getCircumference());
+            //Console.WriteLine(c1.ToString());
 
             Rectangle r1 = new Rectangle();
             r1.setLength(5);
@@ -20,19 +20,23 @@ namespace ConsoleApplication3
             Console.WriteLine("Perimeter of Rectangle is " + r1.getPerimeter());
             Console.WriteLine("Area of Rectangle is " + r1.getArea());
             Console.WriteLine(r1.ToString());
+        
             //   c1.setRadius(5.0);
-            //   c1.setColor("Brown");
-            //   Console.WriteLine("The circle has radius of " 
-            //+ c1.getRadius() + " and area of " + c1.getArea()+ " and color is "+ c1.getColor());
-            //   Console.WriteLine();
-            //   Circle c2 = new Circle(3);
-            //   Console.WriteLine("The circle has radius of " 
-            //+ c2.getRadius() + " and area of " + c2.getArea());
-            //   Console.WriteLine();
-            //   Circle c3 = new Circle(3, "Orange");
-            //   Console.WriteLine("The circle color is " + c3.getColor());
-            //   Console.WriteLine(c3.ToString());
+         //   c1.setColor("Brown");
+         //   Console.WriteLine("The circle has radius of "
+         //+ c1.getRadius() + " and area of " + c1.getArea() + " and color is " + c1.getColor());
+         //   Console.WriteLine();
+         //   Circle c2 = new Circle(3);
+         //   Console.WriteLine("The circle has radius of "
+         //+ c2.getRadius() + " and area of " + c2.getArea());
+         //   Console.WriteLine();
+         //   Circle c3 = new Circle(3, "Orange");
+         //   Console.WriteLine("The circle color is " + c3.getColor());
+         //   Console.WriteLine(c3.ToString());
 
+            //Employee e = new Employee(193,10,"nam","nhat");
+            //e.setSalary(3);
+            //Console.WriteLine(e.getAnnualSalary());
 
 
         }
