2ea905d82c8a68f780c56c503c94f46b06c609c8
quang <thaiquang816@gmail.com>
2019-11-05 11:36:57 +0700

    OOP 2.4-2.5

---
+++ ConsoleApp3/ConsoleApp4/MyCircle.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class MyCircle
+    {
+        private MyPoint center;
+        private int radius = 1;
+        MyCircle()
+        {
+
+        }
+        MyCircle(int x,int y,int radius)
+        {
+            this.radius = radius;
+            center = new MyPoint(x, y);
+        }
+        MyCircle(MyPoint center,int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+        public int getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(int radius)
+        {
+            this.radius = radius;
+            
+        }
+        public MyPoint getColor()
+        {
+            return center;
+        }
+        public void setColor(MyPoint center)
+        {
+            this.center = center;
+        }
+        public int getCenterX()
+        {
+            return center.getX();
+        }
+        public void setCenteX(int x)
+        {
+            center.setX(x);
+        }
+        public int getCenterY()
+        {
+            return center.getY();
+        }
+        public void setCenterY(int y)
+        {
+            center.setY(y);
+        }
+        public int[] getCenterXY()
+        {
+            int[] cexy = new int[2];
+            cexy[0] = this.getCenterX();
+            cexy[1] = this.getCenterY();
+            return cexy;
+        }
+        public void setCenterXY(int x, int y)
+        {
+            setCenteX(x);
+            setCenterY(y);
+        }
+        public override string ToString()
+        {
+            return string.Format("MyCircle[radius = {0},center = {1}", this.radius, this.center);
+            //return base.ToString();
+        }
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public double getCircumference()
+        {
+            return radius * 2 * Math.PI;
+        }
+        public double distance(MyCircle point)
+        {
+            return center.Distance(point.center);
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp4/mypoint.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class MyPoint
+    {
+        private int x = 0;
+        private int y = 0;
+        public MyPoint()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        public MyPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+        public int[] getXY()
+        {
+            int[] xy = new int[2];
+            xy[0] = this.x;
+            xy[1] = this.y;
+            return xy;
+        }
+        public void setXY(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1}", this.x, this.y);
+        }
+        public double Distance(int x, int y)
+        {
+            int deltax = this.x - x;
+            int deltay = this.y - y;
+            return Math.Sqrt(((deltax) * (deltax)) + ((deltay) + (deltay)));
+        }
+        public double Distance(MyPoint point)
+        {
+            return Distance(point.x, point.y);
+        }
+        public double Distance()
+        {
+            return Distance(0, 0);
+        }
+    }
+}
+
